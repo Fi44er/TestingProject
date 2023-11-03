95b41fe8836a2d17554e18c637c810ff7e5daa4e
Fi44er <vaceslavmalugin81@gmail.com>
2023-11-03 10:48:56 +0500

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import { Routes, Route, Link } from "react-router-dom"
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <header>
+        <div className="headerLogo">
+          <img src="/src/assets/logo.svg" alt="logo" />
+          <h1>Тестироввание</h1>
+        </div>
+        <div className="headerMenu">
+          <nav className="headerNav">
+            <ul>
+              <li><Link to="/">Главная</Link></li>
+              <li><Link to="#">О нас</Link></li>
+              <li><Link to="#">Помощь</Link></li>
+              <li><Link to="#">Оренбург</Link></li>
+            </ul>
+          </nav>
+          <div>
+            <p>+7 (3532) 31-72-39</p>
+          </div>
+          <div className="profile">
+            <Link to='#'>Личный кабинет</Link>
+            <img src="/src/assets/profile.svg" alt="profile" />
+          </div>
+        </div>
+      </header>
+      {/* <Routes><Route path="/" element={<Home />} /></Routes> */}
     </>
   )
 }
